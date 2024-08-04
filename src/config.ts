0a10f3c84e185069205fe41845587c74080d4892
Saray Moreno <saraymoreno@192.168.1.10>
2024-08-04 16:57:39 -0300

    build: adding mysql services to yml

--- src/config.ts
+++ src/config.ts
@@ -2,9 +2,16 @@ import dotenv from 'dotenv';
 
 dotenv.config({ path: '.env' });
 
-const CONFIG = {
+interface IConfiguration {
+    DB_USERNAME: string,
+    DB_PASSWORD?: string,
+    DB_PORT?: string,
+}
+
+const CONFIG: IConfiguration = {
     DB_USERNAME: process.env.DB_USERNAME || 'root',
-    DB_PASSWORD: process.env.DB_PASSWORD
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_PORT: process.env.DB_PORT,
 }
 
 export default CONFIG;
