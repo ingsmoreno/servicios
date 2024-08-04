2c105f095a5069b9b0fdc299d561f4dcb77ca6e4
Saray Moreno <saraymoreno@192.168.1.10>
2024-08-04 16:59:21 -0300

    Revert "build: adding mysql services to yml"
    This reverts commit 0a10f3c84e185069205fe41845587c74080d4892.

--- src/config.ts
+++ src/config.ts
@@ -2,16 +2,9 @@ import dotenv from 'dotenv';
 
 dotenv.config({ path: '.env' });
 
-interface IConfiguration {
-    DB_USERNAME: string,
-    DB_PASSWORD?: string,
-    DB_PORT?: string,
-}
-
-const CONFIG: IConfiguration = {
+const CONFIG = {
     DB_USERNAME: process.env.DB_USERNAME || 'root',
-    DB_PASSWORD: process.env.DB_PASSWORD,
-    DB_PORT: process.env.DB_PORT,
+    DB_PASSWORD: process.env.DB_PASSWORD
 }
 
 export default CONFIG;
