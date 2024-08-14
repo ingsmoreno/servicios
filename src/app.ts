fabd6095a3556852e7a2eb2389a136d76afcee24
Saray Moreno <saraymoreno@192.168.1.10>
2024-08-14 14:06:39 -0300

    express application server

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import cookieParser from 'cookie-parser';
+import express from "express";
+import { router as usuariosRouter }  from './usuarios/usuarios.router';
+
+export const app = express();
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' })); // REF: https://heynode.com/tutorial/process-user-login-form-expressjs/
+app.use(cookieParser());
+
+app.use('/api/v1/usuarios', usuariosRouter);  
